48bc919e09fc9bd40cc2097cb9d7ae5d98d06b41
sven <sven@c3d2.de>
2008-07-24 16:55:04 +0200

    add some text

--- dump_plpgsql_function.c
+++ dump_plpgsql_function.c
@@ -1,5 +1,6 @@
 
 #include <postgres.h>
+#include <utils/builtins.h>
 #include <fmgr.h>
 #include <plpgsql.h>
 
@@ -7,11 +8,22 @@
 PG_MODULE_MAGIC;
 #endif
 
-PG_FUNCTION_INFO_V1(plpgsql_dumptree);
+/*
+ * CREATE OR REPLACE FUNCTION dump_plpgsql_function(text) returns text AS '/home/sven/diplom/dump_plpgsql_function/dump_plpgsql_function.so', 'dump_plpgsql_function' LANGUAGE C STRICT;
+ *
+ *
+ */
 
-Datum plpgsql_dumptree( PG_FUNCTION_ARGS )
+extern text * cstring_to_text( char * );
+extern PLpgSQL_function * plpgsql_compile( FunctionCallInfo fcinfo, bool forValidator );
+
+PG_FUNCTION_INFO_V1(dump_plpgsql_function);
+
+Datum dump_plpgsql_function( PG_FUNCTION_ARGS )
 {
-  text *t = PG_GETARG_TEXT_P( 0 );
+  text * t = PG_GETARG_TEXT_P( 0 );
+  t = cstring_to_text( "<xml>" );
+//  t = cstring_to_text( snprintf("1234567812345678",16,"%x",plpgsql_compile) );
   PG_RETURN_TEXT_P( t );
 }
 
