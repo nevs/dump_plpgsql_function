524ca334622a6f0ab9f6bd0eac7dc80d823feb2c
Sven Klemm <sven@c3d2.de>
2008-11-09 02:52:11 +0100

    adding string_helper

---
+++ string_helper.c
@@ -0,0 +1,40 @@
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <postgres.h>
+#include <fmgr.h>
+
+#include "string_helper.h"
+
+/** helper function to append c string to existing c string */
+int 
+__attribute__ ((format (printf, 2, 3)))
+append_string( char ** buffer, char * fmt, ... )
+{
+  va_list ap;
+  char * tmp;
+
+  va_start(ap, fmt);
+  if ( vasprintf(&tmp, fmt, ap) == -1 ) {
+    /* error */
+    return -1;
+  } else {
+    size_t offset = 0;
+    if ( !*buffer ) {
+      *buffer = (char *) palloc( strlen( tmp ) + 1 );
+    } else {
+      offset = strlen( *buffer );
+      *buffer = (char *) repalloc( *buffer, offset + strlen( tmp ) + 1 );
+    }
+    if (!*buffer) {
+      free( tmp );
+      return -1;
+    } else {
+      memcpy( *buffer + offset, tmp, strlen(tmp) + 1 );
+      free( tmp );
+    }
+    return 0;
+  }
+}
---
+++ string_helper.h
@@ -0,0 +1,5 @@
+
+int 
+__attribute__ ((format (printf, 2, 3)))
+append_string( char ** buffer, char * fmt, ... );
+
