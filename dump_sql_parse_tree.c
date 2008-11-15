6af86a03dbb1e9bc3c326825d4fcfacec783d910
Sven Klemm <sven@c3d2.de>
2008-11-15 23:49:42 +0100

    fix TEXT_NODE makro set debug_query_string to get useful error logs sort nodes by their integer values

--- dump_sql_parse_tree.c
+++ dump_sql_parse_tree.c
@@ -7,15 +7,18 @@
 #include "dump_sql_parse_tree.h"
 #include "sql_parsetree_names.h"
 
+extern const char * debug_query_string;
 
 #define CHILD_NODE( type, child ) conditional_child_node( (Node *)((type *)node)->child, context, #child)
-#define TEXT_NODE( type, child ) xml_textnode( context, #child, "%s", (Node *)((type *)node)->child ? : "" )
+#define TEXT_NODE( type, child ) xml_textnode( context, #child, "%s", (char *)((type *)node)->child ? : "" )
 
 bool parse_tree_walker( Node *node, DumpContext * context );
 
 const char * dump_sql_parse_tree_internal( const char * query ) {
   DumpContext * context = palloc0( sizeof( DumpContext ) );
   context->output = palloc0( sizeof( char *));
+
+  debug_query_string = query;
 
   List * parsetree_list = raw_parser( query );
   raw_expression_tree_walker( (Node *) parsetree_list, parse_tree_walker, context );
@@ -48,10 +51,10 @@ bool parse_tree_walker( Node *node, DumpContext * context )
   const char *tagname = NodeTagNames[nodeTag(node)] ? : "node";
 
   switch( nodeTag(node) ) {
-    case T_Integer:
+    case T_Integer:            // 651
       xml_tag( context, tagname, "value", "%d", intVal( node ), NULL );
       break;
-    case T_String:
+    case T_String:             // 653
       xml_tag( context, tagname, "value", "%s", strVal( node ), NULL );
       break;
     default:
@@ -59,7 +62,43 @@ bool parse_tree_walker( Node *node, DumpContext * context )
       xml_tag_open( context, tagname, NULL );
 
       switch( nodeTag(node) ) {
-        case T_SelectStmt:
+        case T_Alias:          // 300
+          TEXT_NODE( Alias, aliasname );
+          CHILD_NODE( Alias, colnames );
+          break;
+        case T_RangeVar:       // 301
+          TEXT_NODE( RangeVar, catalogname );
+          TEXT_NODE( RangeVar, schemaname );
+          TEXT_NODE( RangeVar, relname );
+          CHILD_NODE( RangeVar, alias );
+          break;
+        case T_A_Expr:         // 900
+          xml_tag_open( context, "operator", "type", A_Expr_Kind_Names[((A_Expr *)node)->kind], NULL );
+          CHILD_NODE( A_Expr, name );
+          xml_tag_close( context, "operator" );
+          CHILD_NODE( A_Expr, lexpr );
+          CHILD_NODE( A_Expr, rexpr );
+          break;
+        case T_ColumnRef:      // 901
+          CHILD_NODE( ColumnRef, fields );
+          break;
+        case T_A_Const:        // 903
+          parse_tree_walker((Node *) &(((A_Const *)node)->val), (void *) context );
+          break;
+        case T_FuncCall:       // 904
+          CHILD_NODE( FuncCall, funcname );
+          CHILD_NODE( FuncCall, args );
+          break;
+        case T_A_Indirection:  // 907
+          CHILD_NODE( A_Indirection, arg );
+          CHILD_NODE( A_Indirection, indirection );
+          break;
+        case T_ResTarget:      // 909
+          TEXT_NODE( ResTarget, name );
+          CHILD_NODE( ResTarget, indirection );
+          CHILD_NODE( ResTarget, val );
+          break;
+        case T_SelectStmt:     // 705
           CHILD_NODE( SelectStmt, distinctClause );
           CHILD_NODE( SelectStmt, intoClause );
           CHILD_NODE( SelectStmt, targetList );
@@ -79,41 +118,6 @@ bool parse_tree_walker( Node *node, DumpContext * context )
             CHILD_NODE( SelectStmt, larg );
             CHILD_NODE( SelectStmt, rarg );
           }
-          break;
-        case T_RangeVar:
-          TEXT_NODE( RangeVar, catalogname );
-          TEXT_NODE( RangeVar, schemaname );
-          TEXT_NODE( RangeVar, relname );
-          CHILD_NODE( RangeVar, alias );
-          break;
-        case T_ResTarget:
-          if (((ResTarget *)node)->name) {
-            xml_tag( context, "name", "value", "%s", ((ResTarget *)node)->name, NULL );
-          }
-          CHILD_NODE( ResTarget, indirection );
-          CHILD_NODE( ResTarget, val );
-          break;
-        case T_A_Const: 
-          parse_tree_walker((Node *) &(((A_Const *)node)->val), (void *) context );
-          break;
-        case T_FuncCall:
-          CHILD_NODE( FuncCall, funcname );
-          CHILD_NODE( FuncCall, args );
-          break;
-        case T_A_Expr:
-          xml_tag_open( context, "operator", "type", A_Expr_Kind_Names[((A_Expr *)node)->kind], NULL );
-          CHILD_NODE( A_Expr, name );
-          xml_tag_close( context, "operator" );
-
-          CHILD_NODE( A_Expr, lexpr );
-          CHILD_NODE( A_Expr, rexpr );
-          break;
-        case T_ColumnRef:
-          CHILD_NODE( ColumnRef, fields );
-          break;
-        case T_A_Indirection:
-          CHILD_NODE( A_Indirection, arg );
-          CHILD_NODE( A_Indirection, indirection );
           break;
         default: 
           retval = raw_expression_tree_walker(node, parse_tree_walker, (void *) context );
