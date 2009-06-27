fdcf59c462219503f93549b33e66ea905d09c64b
Sven Klemm <sven@c3d2.de>
2009-06-27 22:18:08 +0200

    fill out all missing places in datum types

--- dump_plpgsql_function.c
+++ dump_plpgsql_function.c
@@ -60,6 +60,7 @@ static void dump_statement( FunctionDumpContext * context, PLpgSQL_stmt *stmt );
 static void dump_datum( FunctionDumpContext * context, PLpgSQL_datum * datum );
 static void dump_exception( FunctionDumpContext * context, PLpgSQL_exception * datum );
 static void dump_datatype( FunctionDumpContext * context, Oid typoid );
+static void dump_tupledesc( FunctionDumpContext * context, TupleDesc tupdesc );
 
 static const char * oid_datatype_name( Oid oid )
 {
@@ -130,6 +131,7 @@ static void dump_datum( FunctionDumpContext * context, PLpgSQL_datum * node )
   switch( node->dtype ) {
     case PLPGSQL_DTYPE_VAR:
       TEXT_NODE( PLpgSQL_var, node, refname );
+      NUMBER_NODE( PLpgSQL_var, node, lineno );
       dump_datatype( context, ((PLpgSQL_var*)node)->datatype->typoid );
       BOOL_NODE( PLpgSQL_var, node, isconst );
       BOOL_NODE( PLpgSQL_var, node, notnull );
@@ -145,63 +147,53 @@ static void dump_datum( FunctionDumpContext * context, PLpgSQL_datum * node )
       break;
     case PLPGSQL_DTYPE_ROW:
       TEXT_NODE( PLpgSQL_row, node, refname );
-      xml_textnode( context->dump, "oid", "%d", ((PLpgSQL_row *)node)->rowtupdesc->tdtypeid );
+      NUMBER_NODE( PLpgSQL_row, node, lineno );
+
+      xml_tag_open( context->dump, "rowtupdesc" );
+      dump_tupledesc( context, ((PLpgSQL_row *)node)->rowtupdesc);
+      xml_tag_close( context->dump, "rowtupdesc" );
+
       xml_tag_open( context->dump, "fields" );
       for( i=0; i < ((PLpgSQL_row *)node)->nfields; i++ ) {
         xml_tag_open( context->dump, "field" );
         xml_textnode( context->dump, "fieldname", "%s", ((PLpgSQL_row *)node)->fieldnames[i] );
-        xml_textnode( context->dump, "oid", "%d", ((PLpgSQL_row *)node)->rowtupdesc->attrs[i]->atttypid );
-        xml_textnode( context->dump, "datatype", "%s", oid_datatype_name(((PLpgSQL_row *)node)->rowtupdesc->attrs[i]->atttypid) );
+        xml_textnode( context->dump, "varno", "%d", ((PLpgSQL_row *)node)->varnos[i] );
         xml_tag_close( context->dump, "field" );
       }
       xml_tag_close( context->dump, "fields" );
       break;
     case PLPGSQL_DTYPE_REC:
       TEXT_NODE( PLpgSQL_rec, node, refname );
-      xml_textnode( context->dump, "oid", "%d", ((PLpgSQL_rec *)node)->tupdesc->tdtypeid );
-      xml_tag_open( context->dump, "fields" );
-      for( i=0; i < ((PLpgSQL_rec *)node)->tupdesc->natts; i++ ) {
-        xml_tag_open( context->dump, "field" );
-        xml_textnode( context->dump, "fieldname", "%s", ((PLpgSQL_rec *)node)->tupdesc->attrs[i]->attname );
-        xml_textnode( context->dump, "oid", "%d", ((PLpgSQL_rec *)node)->tupdesc->attrs[i]->atttypid );
-        xml_textnode( context->dump, "datatype", "%s", oid_datatype_name(((PLpgSQL_rec *)node)->tupdesc->attrs[i]->atttypid) );
-        xml_tag_close( context->dump, "field" );
-      }
-      xml_tag_close( context->dump, "fields" );
+      NUMBER_NODE( PLpgSQL_rec, node, lineno );
+
+      xml_tag_open( context->dump, "tupdesc" );
+      dump_tupledesc( context, ((PLpgSQL_rec *)node)->tupdesc);
+      xml_tag_close( context->dump, "tupdesc" );
+      BOOL_NODE( PLpgSQL_rec, node, freetup );
+      BOOL_NODE( PLpgSQL_rec, node, freetupdesc );
       break;
     case PLPGSQL_DTYPE_RECFIELD:
-      // FIXME incomplete
       TEXT_NODE( PLpgSQL_recfield, node, fieldname );
+      NUMBER_NODE( PLpgSQL_recfield, node, recparentno );
       break;
     case PLPGSQL_DTYPE_ARRAYELEM:
       CHILD_DATUM( PLpgSQL_arrayelem, node, subscript );
       NUMBER_NODE( PLpgSQL_arrayelem, node, arrayparentno );
       break;
     case PLPGSQL_DTYPE_EXPR:
-      // FIXME incomplete
-      if (((PLpgSQL_expr *)node)->query ) {
-        TEXT_NODE( PLpgSQL_expr, node, query );
-        xml_tag_open( context->dump, "params" );
-        for( i=0; i < ((PLpgSQL_expr *)node)->nparams; i++ ) {
-          xml_tag_open( context->dump, "Param" );
-          xml_textnode( context->dump, "index", "%d", ((PLpgSQL_expr *)node)->params[i] );
-          if (((PLpgSQL_expr *)node)->plan) {
-            
-          }
-//          xml_textnode( context->dump, "datatype_oid", "%d", ((PLpgSQL_expr *)node)->plan_argtypes[i] );
-//          xml_textnode( context->dump, "datatype", "%s", oid_datatype_name(((PLpgSQL_expr *)node)->plan_argtypes[i]) );
-          xml_tag_close( context->dump, "Param" );
-        }
-        xml_tag_close( context->dump, "params" );
-      }
-      if ( ((PLpgSQL_expr *)node)->expr_simple_type ) {
-        xml_tag_open( context->dump, "result_type" );
-        dump_datatype( context, ((PLpgSQL_expr *)node)->expr_simple_type );
-        xml_tag_close( context->dump, "result_type" );
-      }
-      
-
+      TEXT_NODE( PLpgSQL_expr, node, query );
       dump_sql_parse_tree_internal( context->dump, ((PLpgSQL_expr *)node)->query );
+
+      if (((PLpgSQL_expr *)node)->expr_simple_expr) {
+        xml_textnode( context->dump, "expr_simple_type", "%d", ((PLpgSQL_expr *)node)->expr_simple_type );
+        xml_textnode( context->dump, "expr_simple_type_name", "%d", oid_datatype_name(((PLpgSQL_expr *)node)->expr_simple_type) );
+      }
+
+      for( i=0; i < ((PLpgSQL_expr *)node)->nparams; i++ ) {
+        xml_tag_open( context->dump, "Param" );
+        xml_textnode( context->dump, "index", "%d", ((PLpgSQL_expr *)node)->params[i] );
+        xml_tag_close( context->dump, "Param" );
+      }
       break;
     case PLPGSQL_DTYPE_TRIGARG:
       CHILD_DATUM( PLpgSQL_trigarg, node, argnum );
@@ -384,6 +376,13 @@ static void dump_statement( FunctionDumpContext * context, PLpgSQL_stmt *node )
 
   };
   xml_tag_close( context->dump, tagname );
+}
+
+static void dump_tupledesc( FunctionDumpContext * context, TupleDesc tupdesc )
+{
+  // FIXME incomplete
+  xml_tag_open( context->dump, "TupleDesc" );
+  xml_tag_close( context->dump, "TupleDesc" );
 }
 
 static void dump_exception( FunctionDumpContext * context, PLpgSQL_exception * datum )
